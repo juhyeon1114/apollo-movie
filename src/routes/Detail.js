af8306ec9d33310b40ed90222881d4ebd5a817a6
unknown <juhyeon1114@gmail.com>
2021-02-03 21:22:40 +0900

    log : 1.2까지 완료

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,120 @@
-import React from 'react';
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import { gql } from "apollo-boost";
+// import { useQuery } from "@apollo/react-hooks";
+// import styled from "styled-components";
 
-export default () => <h1>detail</h1>;
+// const GET_MOVIE = gql`
+//   query getMovie($id: Int!) {
+//     movie(id: $id) {
+//       id
+//       title
+//       medium_cover_image
+//       language
+//       rating
+//       description_intro
+//       isLiked @client
+//     }
+//     suggestions(id: $id) {
+//       id
+//       medium_cover_image
+//     }
+//   }
+// `;
+
+// const Container = styled.div`
+//   height: 100vh;
+//   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+//   width: 100%;
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   color: white;
+// `;
+
+// const Column = styled.div`
+//   margin-left: 10px;
+//   width: 50%;
+// `;
+
+// const Title = styled.h1`
+//   font-size: 65px;
+//   margin-bottom: 15px;
+// `;
+
+// const Subtitle = styled.h4`
+//   font-size: 35px;
+//   margin-bottom: 10px;
+// `;
+
+// const Description = styled.p`
+//   font-size: 28px;
+// `;
+
+// const Poster = styled.div`
+//   width: 25%;
+//   height: 60%;
+//   background-color: transparent;
+//   background-image: url(${props => props.bg});
+//   background-size: cover;
+//   background-position: center center;
+// `;
+
+// const Detail = () => {
+//   const { id } = useParams();
+//   const { loading, data } = useQuery(GET_MOVIE, {
+//     variables: { id: parseInt(id) }
+//   });
+//   return (
+//     <Container>
+//       <Column>
+//         <Title>
+//           {loading
+//             ? "Loading..."
+//             : `${data.movie.title} ${data.movie.isLiked ? "💖" : "😞"}`}
+//         </Title>
+//         <Subtitle>
+//           {data?.movie?.language} · {data?.movie?.rating}
+//         </Subtitle>
+//         <Description>{data?.movie?.description_intro}</Description>
+//       </Column>
+//       <Poster bg={data?.movie?.medium_cover_image}></Poster>
+//     </Container>
+//   );
+// };
+
+// export default Detail;
+
+import {useParams} from 'react-router-dom';
+import {gql} from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) { # for Apollo
+    movie(id: $id) { # gql
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+const Detail = () => {
+  const {id} = useParams();
+  const {loading, data} = useQuery(GET_MOVIE, {
+    variables: {id: +id}
+  });
+  console.log(loading, data)
+
+  if (loading) {
+    return "loading"
+  }
+  if (data && data.movie) {
+    return data.movie.title
+  }
+  return (
+    <h1>Detail {id}</h1>
+  )
+}
+
+export default Detail;
